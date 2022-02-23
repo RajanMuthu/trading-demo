7557836da549998bd46b42139c1373444e0482fa
Gomathipramarajan Muthuchandran <rajan@Rajans-MacBook-Air.local>
2022-02-23 21:42:44 +0530

    Code refactoring

--- src/utility.js
+++ src/utility.js
@@ -1,25 +1,32 @@
+function Trade(price, count, amount) {
+    this.price = price;
+    this.count = count;
+    this.amount = amount;
+    this.total = count * amount;
+}
+
 
 export const getBidsAndAsks = (trades) => {
     const bids = [];
     const asks = [];
     for (const trade of trades) {
-        if (trade[2] > 0) { // if amount is less than 0
-            bids.push({ price: trade[0], count: trade[1], amount: trade[2], total: trade[1] * trade[2] });
-        } else if (trade[2] < 0) {// if amount is greater than 0
-            asks.push({ price: trade[0], count: trade[1], amount: trade[2], total: trade[1] * trade[2] });
+        const [price, count, amount] = trade;
+        const tr = new Trade(price, count, amount);
+        if (amount > 0) {
+            bids.push(tr);
+        } else if (amount < 0) {
+            asks.push(tr);
         }
     }
     return { bids, asks };
 }
 
 export const getTradeUpdate = (updatedTrade) => {
-    const trade = {}
+    let trade = {}
 
     if(updatedTrade && updatedTrade.length >= 2) {
-        trade.price = updatedTrade[0];
-        trade.count = updatedTrade[1];
-        trade.amount = updatedTrade[2];
-        trade.total = updatedTrade[1] * updatedTrade[2];
+        const [price, count, amount] = updatedTrade;
+        trade = new Trade(price, count, amount);
     }
 
     return trade;
